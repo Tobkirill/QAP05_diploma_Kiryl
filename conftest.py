1df0add1eb9996b3feb109b5e4c2ca7ed1124070
Tobkirill <tobkirill@gmail,.com>
2022-04-25 11:32:21 +0300

    added conftest, missed in previous commit

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from pages.login_page import LoginPage
+
+
+@pytest.fixture(scope="function")
+# @pytest.fixture(scope="session")
+def driver():
+    options = Options()
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(options=options)
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.fill_in_form_and_login()
+    yield driver
+    sleep(3)
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+# @pytest.fixture(scope="session")
+def driver_not_logged_in():
+    options = Options()
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    sleep(3)
+    driver.quit()
